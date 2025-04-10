d950429890ccaaee07029a13ab2567388f9f42c1
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2025-04-10 23:10:30 +0700

    ci: configuring sonarqube done

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,14 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "order-service-rizzerve")
+        property("sonar.projectName", "order-service-rizzerve")
+    }
 }
 
 group = "id.ac.ui.cs.advprog"
