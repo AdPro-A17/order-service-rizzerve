690a4238be36649c9e51b6da7a1c9c9b6f8bd90c
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2025-04-10 18:42:57 +0700

    test: configuring OrderServiceApplication test

--- src/test/java/id/ac/ui/cs/advprog/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/orderservice/OrderServiceApplicationTests.java
@@ -2,12 +2,20 @@ package id.ac.ui.cs.advprog.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        OrderServiceApplication.main(new String[] {});
+        assertNotNull(applicationContext);
     }
-
 }
